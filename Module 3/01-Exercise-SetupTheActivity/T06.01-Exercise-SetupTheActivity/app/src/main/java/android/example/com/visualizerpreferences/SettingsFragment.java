33972f23041d02c4b1907a2a8acd1e2889491057
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2019-03-14 17:30:28 +0530

    Module 3

---
+++ Module 3/01-Exercise-SetupTheActivity/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,10 @@
+package android.example.com.visualizerpreferences;
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+public class SettingsFragment extends PreferenceFragmentCompat{
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState,String rootkey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
